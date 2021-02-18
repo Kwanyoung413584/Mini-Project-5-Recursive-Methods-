5375a4af6fd6a8451a73ff681ae32a762d9bf770
Kwanyoung Kim <kwanyoung.kim@patelhs.org>
2021-02-18 15:32:37 +0000

    Comments and added a method which currently doesn't do anything.

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
+// Imports
 import java.util.Scanner;
 
+// Main Class
 class Main {
+
+  // Main Method
   public static void main(String[] args) {
     Scanner Scanner = new Scanner(System.in);
+    int 
 
-    
 
+  }
+
+  // Idk Method
+  public static int rando(int arg1){
+
+    rando(1);
   }
 }
