92447cce22b794e8561719cd51952c5bcaf662a6
KK <kwanyoung.kim@patelhs.org>
2021-02-22 14:21:46 +0000

    Scrapped the other test and begin working on geometrics

--- Main.java
+++ Main.java
@@ -7,68 +7,40 @@ class Main {
   // Main Method
   public static void main(String[] args) {
 
-    String[] Storage;
-
     Scanner Scanner = new Scanner(System.in);
     
-    System.out.println("How many folders can your storage hold?");
+    System.out.println("What number do you want to start from?");
 
-    int storageSize = Scanner.nextInt();
+    int startpoint = Scanner.nextInt();
 
-    Storage = new String[storageSize];
+    System.out.println("What number do you want to multiply by?");
 
-    System.out.println("\nThis is your storage commands, input a specific number to perform an action. Commands are displayed below.\n");
+    int multinum = Scanner.nextInt();
 
-    System.out.println(000 == 0);
-    showCommands();
+    System.out.println("What will be your term? (0 means it will not multiply)");
+
+    int term = Scanner.nextInt();
+
+    int endpoint = geometricSequence(startpoint, multinum, term, 0);
+
+    System.out.println("This will be your number at term number " + term);
+    System.out.println(endpoint);
 
 
   }
 
   // Item Lister Method (Recursive)
-  public static int showDescendants(int arg1){
-
-    showDescendants(1);
-    return 1;
-  }
-
-  // "View" Method
-  public static void viewAction(){
-      System.out.println("Here are your folders, input an number to look inside your folder, otherwise, input 000 to head back to the command list.");
-
-
-
-  }
-
-  // Commands Method
-  public static void showCommands(){
-
-    Scanner Scanner = new Scanner(System.in);
-
-    System.out.println("Storage Commands");
-    System.out.println("----------------");
-    System.out.println("0 View");
-    System.out.println("1 New Folder");
-    System.out.println("2 New File");
-    System.out.println("3 Delete Folder");
-    System.out.println("4 Delete File");
-    System.out.println("5 Edit Folder");
-    System.out.println("6 Edit File");
-
-    int inputCommand = Scanner.nextInt();
-
-    if (inputCommand == 0) {
-
+  public static int geometricSequence(int start, int multi, int term, int startterm){
+    if (startterm <= term) {
+      int calc = start * multi;
+      geometricSequence(calc, multi, term, startterm + 1);
     }
-    else if (inputCommand == 1) {
-
+    else {
+      return start;
     }
-
+    return start;
   }
-
-
 }
 
 
 
-// Idea, folder managing thing
